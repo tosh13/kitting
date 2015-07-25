db414a7f9105df32a9dd6802946f54c9694b25ea
Toshiki Saito <tosh@Musashi.local>
2015-07-25 20:58:24 +0900

    2nd commit

--- cask.sh
+++ cask.sh
@@ -1,5 +1,5 @@
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-hash -r
+# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# hash -r
 
 brew update
 brew upgrade brew-cask
