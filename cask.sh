63f70cbf5e81c5c9985ef6711058a8cbf7d49fdf
Toshiki Saito <tosh@chorus.local>
2015-07-27 05:24:30 +0900

    keep sudo alive

--- cask.sh
+++ cask.sh
@@ -12,8 +12,10 @@ source ~/.bash_profile
 # Ask for the administrator password upfront
 sudo -v
 
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # versioned
-# karabiner needs password
 brew cask install karabiner
 brew cask install 1password
 brew cask install adobe-reader
