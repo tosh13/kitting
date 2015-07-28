a2ff64a8ebe6f4e59a9edbc6e65233067a5b3a66
Toshiki Saito <tosh@Musashi.local>
2015-07-28 23:30:46 +0900

    typo

--- kitting.sh
+++ kitting.sh
@@ -6,9 +6,6 @@ sudo sh -c 'echo "\nDefaults timestamp_timeout=-1">>/etc/sudoers'
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until script has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # String selectable on QuickLook
 defaults write com.apple.finder QLEnableTextSelection -bool yes
 
