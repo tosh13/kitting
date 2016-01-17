4a63508953d6f69d69b9efdd4217bf4d763829eb
Toshiki Saito <github@jdbase.com>
2016-01-17 15:47:21 +0900

    Create versioned.sh

---
+++ versioned.sh
@@ -0,0 +1,22 @@
+brew update
+brew upgrade
+brew cleanup
+brew cask update
+
+brew cask install 1password
+brew cask install adobe-reader
+brew cask install atom
+brew cask install clipmenu
+brew cask install diffmerge
+brew cask install firefox
+brew cask install grandperspective
+brew cask install iterm2
+brew cask install karabiner
+brew cask install kindle
+brew cask install kitematic
+brew cask install omnifocus
+brew cask install slack
+brew cask install textwrangler
+brew cask install the-unarchiver
+brew cask install vagrant
+brew cask install virtualbox
